96f0618aee9e49de3fd33561f17ea9a317493613
betterflowers <1054952496@qq.com>
2020-08-28 23:50:07 +0800

    [karol][employee.js] refactor : extract this.employeeType.includes(type)

--- src/employee.js
+++ src/employee.js
@@ -1,16 +1,18 @@
 class Employee {
     constructor (name, type) {
-        this.validateType(type);
-        this._name = name;
-        this._type = type;
-    }
-
-    validateType (type) {
-        if (![
+        this.employeeType = [
             'engineer',
             'manager',
             'salesman',
-        ].includes(type)) {
+        ];
+        this.validateType(type);
+        this._name = name;
+        this._type = type;
+
+    }
+
+    validateType (type) {
+        if (!this.employeeType.includes(type)) {
             throw new Error(`Employee cannot be of type ${type}`);
         }
     }
