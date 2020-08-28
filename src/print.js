1bc90caeebf5540b5da499056b2e8d7acee9609b
betterflowers <1054952496@qq.com>
2020-08-28 23:35:04 +0800

    [karol] refactor:extract printInit function

---
+++ src/print.js
@@ -0,0 +1,24 @@
+function printInit() {
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
+}
+
+function printOwing (invoice) {
+    let outstanding = 0;
+    printInit();
+
+    // calculate outstanding
+    for (const o of invoice.borderSpacing) {
+        outstanding += o.amount;
+    }
+
+    // record due date
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    // print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+}
