aba183c53c5939b9774069ad7371797a2d27e82f
betterflowers <1054952496@qq.com>
2020-08-28 21:41:23 +0800

    [karol] test: add all rankTest case about rating return value

--- test/rankTest.js
+++ test/rankTest.js
@@ -561,3 +561,93 @@ rankTest('test8 : voyageProfitFactor history length < 10 and zone==west-indies a
 
     t.is(result, 3);
 })
+
+rankTest('test1: rating return A', t => {
+    const voyage = {
+        zone: 'china',
+        length: 13,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        },
+        {
+            zone: 'west-indies',
+            profit: 15,
+        },
+        {
+            zone: 'east-indies',
+            profit: 2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'west-indies',
+            profit: 15,
+        },
+        {
+            zone: 'china',
+            profit: 7,
+        },
+        {
+            zone: 'west-indies',
+            profit: 15,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        }
+    ];
+
+    const result = rating(voyage, history)
+
+    t.is(result, 'A');
+})
+
+rankTest('test2: rating return B', t => {
+    const voyage = {
+        zone: 'west-indies',
+        length: 15,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        },
+        {
+            zone: 'west-indies',
+            profit: 15,
+        },
+        {
+            zone: 'east-indies',
+            profit: 2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+        {
+            zone: 'west-indies',
+            profit: 15,
+        }
+    ];
+
+    const result = rating(voyage, history)
+
+    t.is(result, 'B');
+})
